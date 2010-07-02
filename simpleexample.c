027cd82144b102ef5aee57d5aa0ea63a39887c3f
Eric Smith <eric@8thlight.com>
2010-07-02 01:20:37 -0500

    Okay I have a perspective example now.

--- simpleexample.c
+++ simpleexample.c
@@ -2,8 +2,10 @@
 #include <OpenGL/glu.h>
 #include <Glut/glut.h>
 
-#define kWindowWidth 400
-#define kWindowHeight 300
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_X 100
+#define WINDOW_Y 100
 
 typedef GLfloat point[3]; 
 
@@ -12,8 +14,9 @@ point v[4] ={{0.0, 0.0, 1.0},
              {-0.816497, -0.471405, -0.333333}, 
              {0.816497, -0.471405, -0.333333}};
 
-
-GLvoid DrawGLScene(void);
+void DrawGLScene(void);
+void ChangeSize(GLsizei w, GLsizei h);
+void drawCircles(void);
 void tetrahedron(void);
 void triangle(point a, point b, point c);
 
@@ -21,38 +24,67 @@ int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(kWindowWidth, kWindowHeight);
-  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+  glutInitWindowPosition(WINDOW_X, WINDOW_Y);
   glutCreateWindow("simple opengl example");
 
   glutDisplayFunc(DrawGLScene);
+  // NEW
+  glutReshapeFunc(ChangeSize);
 
   glutMainLoop();
   return 0;
 }
 
-GLvoid DrawGLScene(void)
+void DrawGLScene(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT); 
-  glColor3f(1.0, 1.0, 1.0); 
-  glLoadIdentity(); 
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-  tetrahedron(); 
+  glTranslatef(0.0f, 0.0f, -100.0f);
+
+  drawCircles(); 
+  
   glutSwapBuffers();
 }
 
-void tetrahedron(void) 
-{ 
-  triangle(v[0], v[1], v[2]); 
-  triangle(v[3], v[2], v[1]); 
-  triangle(v[0], v[2], v[3]); 
-} 
+void ChangeSize(GLsizei w, GLsizei h)
+{
+  GLfloat nRange = 100.0f;
 
-void triangle( point a, point b, point c) 
-{ 
-  glBegin(GL_LINE_LOOP);
-    glVertex3fv(a); 
-    glVertex3fv(b); 
-    glVertex3fv(c); 
-  glEnd(); 
+  // Prevent a divide by zero
+  if(h == 0)
+    h = 1;
+
+  // Change the viewport each time the window is resized
+  // otherwise the image can be distorted.
+  // You do not have to match the window size - that's just simple.
+  glViewport(0, 0, w, h);
+
+  // Reset coordinate system
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  // Establish clipping volume (left, right, bottom, top, near, far)
+  if (w <= h) 
+    glOrtho (-nRange, nRange, -nRange*h/w, nRange*h/w, -nRange*2.0f, nRange*2.0f);
+  else 
+    glOrtho (-nRange*w/h, nRange*w/h, -nRange, nRange, -nRange*2.0f, nRange*2.0f);
+
+  //gluPerspective(60.0f, w/h, 1.0, 400.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void drawCircles(void)
+{
+  glColor3ub(255, 0, 0);
+  glutSolidSphere(10.0f, 15, 15);
+  glPushMatrix();
+
+  glTranslatef(20.0f, 0.0f, -40.0f);
+  glColor3ub(255, 255, 0);
+  glutSolidSphere(10.0f, 15, 15);
+  glPopMatrix();
 }
