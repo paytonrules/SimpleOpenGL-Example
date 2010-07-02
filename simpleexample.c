c85c44d06fd7c945889ee689848325e6c9730860
Eric Smith <eric@8thlight.com>
2010-07-01 22:47:30 -0500

    Some seemingly random changes.

--- simpleexample.c
+++ simpleexample.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(kWindowWidth, kWindowHeight);
   glutInitWindowPosition(100, 100);
-  glutCreateWindow ("simple opengl example");
+  glutCreateWindow("simple opengl example");
 
   glutDisplayFunc(DrawGLScene);
 
@@ -34,20 +34,11 @@ int main(int argc, char **argv)
 GLvoid DrawGLScene(void)
 {
   glClear(GL_COLOR_BUFFER_BIT); 
-  glColor3f(1.0,1.0,1.0); 
+  glColor3f(1.0, 1.0, 1.0); 
   glLoadIdentity(); 
 
   tetrahedron(); 
   glutSwapBuffers();
-}
-
-void triangle( point a, point b, point c) 
-{ 
-  glBegin(GL_LINE_LOOP);
-  glVertex3fv(a); 
-  glVertex3fv(b); 
-  glVertex3fv(c); 
-  glEnd(); 
 }
 
 void tetrahedron(void) 
@@ -56,3 +47,12 @@ void tetrahedron(void)
   triangle(v[3], v[2], v[1]); 
   triangle(v[0], v[2], v[3]); 
 } 
+
+void triangle( point a, point b, point c) 
+{ 
+  glBegin(GL_LINE_LOOP);
+    glVertex3fv(a); 
+    glVertex3fv(b); 
+    glVertex3fv(c); 
+  glEnd(); 
+}
